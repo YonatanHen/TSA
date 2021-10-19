5e506092696e547400fc69e3b5644b520b20c9a1
YehonatanHen <yonatan.h333@gmail.com>
2021-10-19 11:35:42 +0300

    Tutor items added

--- components/List Items/TutorSearchItem.js
+++ components/List Items/TutorSearchItem.js
@@ -4,16 +4,29 @@ import { Image, StyleSheet, TouchableOpacity, View, Text } from 'react-native'
 const TutorItem = props => {
 
     return (
-        <View>
-            <Text>
-                I am tutor!
-            </Text>
+        <View style={styles.itemContainer}>
+            <View style={styles.item}>
+                <Image style={{ width: 40, height: 40 , borderRadius: 100}} 
+                source={props.userImage ? { uri: props.userImage } : require('../../images/Default-Profile-Picture.png')} />
+                <Text>
+                    I am tutor!
+                </Text>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    itemContainer: {
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: '#69665e'
+    },
+    item: {
+        flexDirection: 'row',
+        paddingVertical: 10,
+        paddingHorizontal: 7
+    }
 })
 
 export default TutorItem
