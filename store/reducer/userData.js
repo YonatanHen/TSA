106fc28c18cd524e2a69673811770f761e744d8c
YehonatanHen <yonatan.h333@gmail.com>
2021-10-25 21:35:17 +0300

    fixed signup image url problem

--- store/reducer/userData.js
+++ store/reducer/userData.js
@@ -63,7 +63,7 @@ export default (state = initialState, action) => {
                 bio: action.bio,
                 courses: action.courses,
                 phone: action.phone,
-                imageUrl: action.image,
+                imageUrl: action.imageUrl,
                 locationCords: action.locationCords,
                 city: action.city,
                 country: action.country,
