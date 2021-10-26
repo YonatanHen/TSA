b66b1c478efd5f3840898af74fb176102e1754de
YehonatanHen <yonatan.h333@gmail.com>
2021-10-26 13:24:20 +0300

    fixed the problem with app direction

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { I18nManager } from 'react-native'
 import { Provider } from 'react-redux'
 
 import AppNavigator from './navigation/AppNavigator';
 
 import store from './store/store'
 import {fetchInstitutes,readAllUsers} from './store/actions/representation'
+
+I18nManager.forceRTL(false);
+I18nManager.allowRTL(false);
 
 store.dispatch(readAllUsers())
 store.dispatch(fetchInstitutes())
