6f072dff7a1242d9e39a87f446913b6717ad33db
YehonatanHen <yonatan.h333@gmail.com>
2021-11-04 02:14:04 +0200

    Added upcoming lessons to main page screen

--- screens/student/MainPageScreen.js
+++ screens/student/MainPageScreen.js
@@ -1,17 +1,45 @@
 import React from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { DrawerActions } from '@react-navigation/native';
+import { Card, Title, Paragraph } from 'react-native-paper'
 
 import HeaderButton from '../../components/buttons/HeaderButton';
 import AgendaCalendar from '../../components/calendar/agendaCalendar';
+import { useSelector } from 'react-redux';
 
 const MainPage = props => {
+    const loggedInUser = useSelector(state => state.data)
 
     return (
-        <Text>
-            Welcome to the Students Scheduler app!
-        </Text>
+        <View style={{ alignItems: 'center' }}>
+            {loggedInUser.lessons !== {} ? (
+                <ScrollView>
+                    <View style={{ alignItems: 'center' }}>
+                        <Text style={styles.title}>Upcoming Lessons</Text>
+                    </View>
+                    {Object.entries(loggedInUser.lessons).map(date => {
+                        return Object.entries(date[1]).map(lesson => {
+                            console.log(lesson)
+                            return (
+                                <View key={`${date[0]} - ${lesson[0]}`} style={{ alignItems: 'center', marginTop: 5 }}>
+                                    <Card>
+                                        <Card.Content style={{ alignItems: 'center' }}>
+                                            <Title>{date[0]} at {lesson[0]}</Title>
+                                            <Paragraph style={{ fontWeight: '600'}}>
+                                                {lesson[1].tutor} - {lesson[1].course}
+                                            </Paragraph>
+                                        </Card.Content>
+                                    </Card>
+                                </View>
+                            )
+                        })
+                    })}
+                </ScrollView>
+            ) : (
+                <Text style={styles.title}>No lessons planned.</Text>
+            )}
+        </View>
     )
 }
 
@@ -31,6 +59,12 @@ export const screenOptions = navData => {
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    title: {
+        color: 'dodgerblue',
+        fontSize: 26,
+        fontWeight: 'bold'
+    }
+})
 
 export default MainPage
