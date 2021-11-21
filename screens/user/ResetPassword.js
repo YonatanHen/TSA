789d14b3793e00d86a5d729d41687cb87687f73a
YehonatanHen <yonatan.h333@gmail.com>
2021-11-21 17:42:18 +0200

    ui

--- screens/user/ResetPassword.js
+++ screens/user/ResetPassword.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { Button, View, StyleSheet, TextInput, Alert } from 'react-native'
-import Input from '../../components/Inputs/LoginAndSignUp/Input'
 
 const ResetPassword = props => {
     [passwords, setPasswords] = useState({
@@ -8,17 +7,13 @@ const ResetPassword = props => {
         confirmedPassword: ''
     })
 
-    useEffect(()=> {
-        console.log(passwords)
-    })
-
     const submitHandler = () => {
         const passwordRegex = /^(?=.*[A-Za-z])(?=.*[0-9]).*$/
-        if(passwords.newPassword !== passwords.confirmedPassword) {
-            Alert.alert('Error!','Passwords does not match')
+        if (passwords.newPassword !== passwords.confirmedPassword) {
+            Alert.alert('Error!', 'Passwords does not match')
         }
-        if (!passwordRegex.test(passwords.newPassword) || (passwords.newPassword)) {
-            Alert.alert('Error!','Password must contain 6 characters that includes numbers and letters')
+        else if (!passwordRegex.test(passwords.newPassword) || (passwords.newPassword)) {
+            Alert.alert('Error!', 'Password must contain 6 characters that includes numbers and letters')
         }
     }
 
@@ -26,36 +21,42 @@ const ResetPassword = props => {
 
         <View style={styles.inputForm}>
             <TextInput
+                style={styles.input}
                 required
                 placeholder="New password"
                 keyboardType="default"
                 initialValue=''
-                onChangeText={(text) => setPasswords({...passwords, newPassword: text})}
+                onChangeText={(text) => setPasswords({ ...passwords, newPassword: text })}
             />
 
             <TextInput
+                style={styles.input}
                 required
                 placeholder="Confirm the new password"
                 keyboardType="default"
                 initialValue=''
-                onChangeText={(text) => setPasswords({...passwords, confirmedPassword: text})}
+                onChangeText={(text) => setPasswords({ ...passwords, confirmedPassword: text })}
             />
-            <Button title='Reset Password' onPress={submitHandler}/>
+            <View style={{ marginTop: 10 }}>
+                <Button title='Reset Password' onPress={submitHandler} color="deepskyblue"/>
+            </View>
         </View>
     )
 }
 
 
-styles = StyleSheet.create({
-    screen: {
-        flex: 1
-    },
-    label: {
-        fontSize: 20
-    },
+const styles = StyleSheet.create({
     inputForm: {
         width: '100%',
         padding: 10,
+    },
+    input: {
+        // flex: 1,
+        paddingHorizontal: 2,
+        paddingVertical: 10,
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 2,
+        marginBottom: 2
     },
 })
 
